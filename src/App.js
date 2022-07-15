8d9df07d1936497dd7422f4ef06b925ed291bfa6
Omar EL ATMANI <contact@omaghd.com>
2022-07-15 03:43:24 +0100

    Change primary color

--- src/App.js
+++ src/App.js
@@ -15,27 +15,42 @@ import Settings from "./pages/settings";
 import PublicProfile from "./pages/public-profile";
 import NotFound from "./pages/not-found";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0d9488",
+    },
+  },
+});
+
 const App = () => (
-  <AuthContextProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace={true} />} />
-        <Route element={<AuthLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
-        <Route element={<RequireAuth />}>
-          <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/settings" element={<Settings />}></Route>
+  <ThemeProvider theme={theme}>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to="/dashboard" replace={true} />}
+          />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Route>
-        </Route>
-        <Route path=":username" element={<PublicProfile />} />
-        <Route path="/404" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </AuthContextProvider>
+          <Route element={<RequireAuth />}>
+            <Route element={<DashboardLayout />}>
+              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/settings" element={<Settings />}></Route>
+            </Route>
+          </Route>
+          <Route path=":username" element={<PublicProfile />} />
+          <Route path="/404" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
+  </ThemeProvider>
 );
 
 export default App;
